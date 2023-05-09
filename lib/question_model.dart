d24d332acabcd9cda2ee781dacfa97620f332527
NITIN SINGH <nitin2201singh@gmail.com>
2023-05-09 18:38:34 +0530

    quiz app

---
+++ lib/question_model.dart
@@ -0,0 +1,58 @@
+class Question {
+  final String questionText;
+  final List<Answer> answersList;
+
+  Question(this.questionText, this.answersList);
+}
+
+class Answer {
+  final String answerText;
+  final bool isCorrect;
+
+  Answer(this.answerText, this.isCorrect);
+}
+
+List<Question> getQuestions() {
+  List<Question> list = [];
+  //ADD questions and answer here
+
+  list.add(Question(
+    "Grand Central Terminal, Park Avenue, New York is the world's?",
+    [
+      Answer("largest railway station", true),
+      Answer("highest railway station", false),
+      Answer("longest railway station", false),
+      Answer("None of the above", false),
+    ],
+  ));
+
+  list.add(Question(
+    "Who owns Iphone?",
+    [
+      Answer("Apple", true),
+      Answer("Microsoft", false),
+      Answer("Google", false),
+      Answer("Nokia", false),
+    ],
+  ));
+
+  list.add(Question(
+    "Youtube is _________  platform?",
+    [
+      Answer("Music Sharing", false),
+      Answer("Video Sharing", false),
+      Answer("Live Streaming", false),
+      Answer("All of the above", true),
+    ],
+  ));
+
+  list.add(Question(
+    "Flutter user dart as a language?",
+    [
+      Answer("True", true),
+      Answer("False", false),
+    ],
+  ));
+
+  return list;
+}
